1b7bd3b7fbdb19531e91f66fa44df6b47c2f50c3
Dominic <deathmilex@gmail.com>
2026-04-13 00:58:10 -0500

    VarGlobals.ts Añadido

---
+++ src/app/VarGlobals.ts
@@ -0,0 +1,12 @@
+export const GlobalVariable = {
+
+  vusu: '',
+
+  //Base URL BackEnd - ic.backend.precotex.web - HM
+  //baseUrlProcesoTenido: "https://localhost:7093/api/", //LOCAL
+  //baseUrlProcesoTenido: "https://gestion.precotex.com:444/ubicaciones/api/", // PRODUCCION EIQ
+
+
+  Global_menu: {}
+}
+
